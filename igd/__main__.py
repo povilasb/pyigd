d7bb34982322c8a1dcfd525ba70c7f5bfb29c98e
Povilas Balciunas <balciunas90@gmail.com>
2017-12-05 21:11:26 +0200

    Implement CLI options for 'add' command

--- igd/__main__.py
+++ igd/__main__.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import curio
 import click
 
@@ -26,9 +28,31 @@ def ip():
     short_help='Add new port mapping.',
     help='Adds new port mapping.',
 )
-def add():
-    mapping = proto.PortMapping('', 5000, 6000, 'TCP', '192.168.1.10',
-                                True, 'test mapping', 20)
+@click.option('--ext-port', '-e', 'external_port', type=int, required=True,
+              help='External port.',)
+@click.option('--int-port', '-i', 'internal_port', type=int,
+              help='Internal port. If unspecified, the same as --ext-port is '\
+                   'used.',)
+@click.option('--ip', 'ip', type=str, required=False,
+              help='Local IP that port will be forwarded to. If not '\
+                   'specified, this computer IP will be used.',)
+@click.option('--protocol', '-p', 'protocol', type=str, default='TCP',
+              show_default=True,
+              help='UDP or TCP. Mixed letter case allowed.',)
+@click.option('--description', '-t', 'description', type=str, default='pyigd',
+              show_default=True,
+              help='Port mapping description.',)
+@click.option('--duration', '-d', 'duration', type=int, default=0,
+              show_default=True,
+              help='Port mapping duration. If unspecified, mapping stays '\
+              'until IGD/router reboot.',)
+def add(external_port: int, internal_port: Optional[int], ip: Optional[str],
+        protocol: str,
+        description: str, duration: int):
+    internal_port = internal_port or external_port
+    mapping = proto.PortMapping(
+        '', internal_port, external_port, protocol, ip, True, description,
+        duration)
     curio.run(core.add_port_mapping, mapping)
 
 
