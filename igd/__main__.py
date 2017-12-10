daa96f23a566264c13ed5782e792b95b1934846e
Povilas Balciunas <balciunas90@gmail.com>
2017-12-10 15:05:09 +0200

    Mek port number positional argument
    It's a little more ergonomic this way.

--- igd/__main__.py
+++ igd/__main__.py
@@ -61,10 +61,9 @@ def add(external_port: int, internal_port: Optional[int], ip: Optional[str],
     help='Removes port mapping that matches given filters. If protocol is '\
          'not specified, mappings for both UDP and TCP are removed.',
 )
-@click.option('--ext-port', '-e', 'external_port', type=int, required=True,
-              help='External port.',)
 @click.option('--protocol', '-p', 'protocol', type=str,
               help='UDP or TCP. Mixed letter case allowed.',)
+@click.argument('external_port', type=int, required=True,)
 def rm(external_port: int, protocol: Optional[str]):
     curio.run(core.delete_port_mapping, external_port, protocol)
 
