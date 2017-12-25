a0c288fe6bbd47f3f783f316a2cead8118355527
Povilas Balciunas <balciunas90@gmail.com>
2017-12-25 17:58:00 +0200

    Fix AsyncMock
    Allow async_side_effect constructor parameter.

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -5,3 +5,4 @@ class AsyncMock(AIOMock):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.async_return_value = kwargs.get('async_return_value')
+        self.async_side_effect = kwargs.get('async_side_effect')
